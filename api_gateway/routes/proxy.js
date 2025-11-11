8d64bd45167def16498d304d0af621d451ab6963
m1khasik <146825853+m1khasik@users.noreply.github.com>
2025-11-11 13:19:19 +0300

    Update proxy.js

--- api_gateway/routes/proxy.js
+++ api_gateway/routes/proxy.js
@@ -1,1 +1,32 @@
+// api_gateway/src/routes/proxy.js
+const proxy = require('express-http-proxy');
 
+const USERS_SERVICE = process.env.USERS_SERVICE_URL || 'http://service_users:3001';
+const ORDERS_SERVICE = process.env.ORDERS_SERVICE_URL || 'http://service_orders:3002';
+
+function safeHeader(value) {
+  return value == null ? '' : String(value);
+}
+
+function setupProxy(app) {
+  // Проксируем /v1/users → в USERS_SERVICE, сохраняя путь
+  app.use('/v1/users', proxy(USERS_SERVICE, {
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers['x-request-id'] = safeHeader(srcReq.id);
+      proxyReqOpts.headers['x-user-id'] = safeHeader(srcReq.userId);
+      proxyReqOpts.headers['x-user-roles'] = safeHeader(JSON.stringify(srcReq.userRoles || []));
+      return proxyReqOpts;
+    }
+  }));
+
+  app.use('/v1/orders', proxy(ORDERS_SERVICE, {
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers['x-request-id'] = safeHeader(srcReq.id);
+      proxyReqOpts.headers['x-user-id'] = safeHeader(srcReq.userId);
+      proxyReqOpts.headers['x-user-roles'] = safeHeader(JSON.stringify(srcReq.userRoles || []));
+      return proxyReqOpts;
+    }
+  }));
+}
+
+module.exports = setupProxy;
